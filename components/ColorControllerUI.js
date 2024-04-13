befca95d7f168f7ca3ee1ea1126d374747d6bb82
Md. Shahriar Mehedi <sau.shahriar@gmail.com>
2024-04-13 07:23:57 +0600

    2nd stage done

--- components/ColorControllerUI.js
+++ components/ColorControllerUI.js
@@ -65,25 +65,60 @@ const ColorControllerUI = () => {
     const hexColor = `#${((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1)}`;
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div className="flex items-center space-y-8 flex-wrap">
-                <ColorSlider color="Red" value={red} onChange={(value) => handleSliderChange('Red', value)} />
-                <ColorSlider color="Green" value={green} onChange={(value) => handleSliderChange('Green', value)} />
-                <ColorSlider color="Blue" value={blue} onChange={(value) => handleSliderChange('Blue', value)} />
-                <ColorSlider color="Black" value={black} onChange={(value) => handleSliderChange('Black', value)} />
-                <ColorSlider color="Yellow" value={yellow} onChange={(value) => handleSliderChange('Yellow', value)} />
-                <ColorSlider color="Cyan" value={cyan} onChange={(value) => handleSliderChange('Cyan', value)} />
-                <ColorSlider color="Magenta" value={magenta} onChange={(value) => handleSliderChange('Magenta', value)} />
+        <div className="h-screen">
+
+            <div className="flex items-center gap-5 justify-center mt-10 lg:mt-20 scale-90 lg:scale-100">
+                <div
+                    className=" w-40 h-40 mt-8 "
+                    style={{
+                        backgroundColor: outputColor,
+                        border: '1px solid black',
+                    }}
+                ></div>
+                <div
+                    className=" w-40 h-40 mt-8 "
+                    style={{
+                        backgroundColor: outputColor,
+                        border: '1px solid black',
+                    }}
+                ></div>
+                <div
+                    className=" w-40 h-40 mt-8 "
+                    style={{
+                        backgroundColor: outputColor,
+                        border: '1px solid black',
+                    }}
+                ></div>
             </div>
-            <div
-                className="w-32 h-32 mt-8"
-                style={{
-                    backgroundColor: outputColor,
-                    border: '1px solid black',
-                }}
-            ></div>
-            <p className="mt-4">RGB: {outputColor}</p>
-            <p>Hex: {hexColor}</p>
+            <div className="flex flex-col justify-between bg-[#C4C4C4] pt-10 rounded-2xl scale-90 lg:scale-100 lg:absolute lg:bottom-20 lg:right-20">
+                <div className="flex items-center  flex-wrap ">
+                    <ColorSlider svg="/GS_handle.svg" color="white" value={black} onChange={(value) => handleSliderChange('Black', value)} />
+                    <ColorSlider svg="/R handle.svg" color="Red" value={red} onChange={(value) => handleSliderChange('Red', value)} />
+                    <ColorSlider svg="/Y_handle.svg" color="Yellow" value={yellow} onChange={(value) => handleSliderChange('Yellow', value)} />
+                    <ColorSlider svg="/G_handle.svg" color="Green" value={green} onChange={(value) => handleSliderChange('Green', value)} />
+                    <ColorSlider svg="/C_handle.svg" color="Cyan" value={cyan} onChange={(value) => handleSliderChange('Cyan', value)} />
+                    <ColorSlider svg="/B_handle.svg" color="Blue" value={blue} onChange={(value) => handleSliderChange('Blue', value)} />
+                    <ColorSlider svg="/M_handle.svg" color="Magenta" value={magenta} onChange={(value) => handleSliderChange('Magenta', value)} />
+                </div>
+                <div className="flex justify-center items-center gap-1 pt-7">
+                    <div className='bg-[#A9A9A9] text-white text-center w-1/2 p-3 rounded-bl-2xl'>
+                        <h2>
+                            {
+                                `${red},${green},${blue}`
+                            }
+                        </h2>
+                    </div>
+                    <div className='bg-[#A9A9A9] text-white text-center w-1/2 p-3 rounded-br-2xl'>
+                        <h2>
+                            {
+                                //hexadecimal value in 0x000000 format 
+                                `0x${((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1).toUpperCase()}`
+                            }
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- components/ColorSlider.js
+++ components/ColorSlider.js
@@ -1,24 +1,73 @@
 'use client'
 
-import { useState } from 'react';
+import Image from 'next/image';
+import { useState, useRef, useEffect } from 'react';
 
-const ColorSlider = ({ color, value, onChange }) => {
+const ColorSlider = ({ svg, color, value, onChange }) => {
+    const [dragging, setDragging] = useState(false);
+    const sliderRef = useRef(null);
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            if (dragging) {
+                const rect = sliderRef.current.getBoundingClientRect();
+                let newValue = Math.round(((rect.bottom - e.clientY) / rect.height) * 255);
+                newValue = Math.min(Math.max(newValue, 0), 255); // Ensure value is within bounds
+                onChange(newValue);
+            }
+        };
+
+        const handleMouseUp = () => {
+            setDragging(false);
+        };
+
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [dragging, onChange]);
+
+    const handleMouseDown = (e) => {
+        setDragging(true);
+        e.preventDefault(); // Prevent default behavior to avoid selection of the icon text
+    };
+
+    const sliderPosition = ((255 - value) / 255) * 100;
+
     return (
         <div className="flex flex-col items-center space-y-2">
-            <p className="text-lg font-bold">{color}</p>
-            <input
-                type="range"
-                min="0"
-                max="255"
-                value={value}
-                onChange={(e) => onChange(parseInt(e.target.value))}
-                className="slider-vertical"
-                style={{ writingMode: 'bt-lr', WebkitAppearance: 'slider-vertical', height: '10rem' }}
-            />
-            <p>{value}</p>
+            {/* <p className="text-lg font-bold">{color}</p> */}
+            <div
+                ref={sliderRef}
+                className="h-64 w-12 relative cursor-pointer flex flex-col items-center "
+                onMouseDown={handleMouseDown}
+            >
+                <div
+                    style={{ background: `linear-gradient(to top, black, ${color})` }}
+                    className=' h-full w-[3px]'>
+
+                </div>
+                <div
+                    className="absolute top-0 left-1/2 transform -translate-x-1/2 "
+                    style={{ top: `${sliderPosition}%`, pointerEvents: dragging ? 'none' : 'auto' }}
+                >
+                    <Image
+                        src={svg}
+                        alt="Slider Icon"
+                        onMouseDown={handleMouseDown} // This is to start dragging when clicking the icon
+                        className="w-8 h-8"
+                        style={{ marginTop: '-1rem' }}
+                        width={32}
+                        height={32}
+                    />
+                </div>
+            </div>
+            {/* <p>{value}</p> */}
         </div>
     );
 };
 
 export default ColorSlider;
-
